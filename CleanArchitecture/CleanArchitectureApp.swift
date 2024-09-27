9a36ed2aae3d495b13bc5e7b66a148780539ac47
StepanFriedl <stepanfriedl13@gmail.com>
2024-09-27 11:12:34 +0200

    Initializing a new git repository.

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Štěpán Friedl on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
